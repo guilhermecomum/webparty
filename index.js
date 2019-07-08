d858e3308d924cf14adc1d1c48f2f484d5e3e22a
Guilherme Guerra <guilherme.ga@gmail.com>
2019-07-08 20:27:01 -0300

    Adds sounds with beet.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use('/assets', express.static('assets'))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -10,14 +13,31 @@ app.get('/admin', function(req, res){
     res.sendFile(__dirname + '/admin.html');
 });
 
+
+var clients = [];
+
 io.on('connection', function(socket){
+    clients.push(socket.id);
+
+    socket.on('disconnect', function(){
+        clients.pop(socket.id);
+    });
+
     socket.on('up', function() {
         io.emit('up');
     })
 
     socket.on('down', function() {
         io.emit('down');
-    })        
+    })
+
+    socket.on('sound', function() {
+        io.emit('sound');
+    })
+
+    socket.on('getClients', function() {
+        io.emit('clients', clients)
+    })
 
 });
 
